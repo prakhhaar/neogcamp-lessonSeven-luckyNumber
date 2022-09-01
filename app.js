0731debac3080c8bb7babd13d7c64049167b5bcf
Prakhar Sharma <prakhar.shrm202@gmail.com>
2022-09-02 02:28:15 +0530

    update: calculated sum of DOB

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumber = document.querySelector("#check-number");
 
-checkNumber.addEventListener("click", function getValues(){
-    console.log(Number(dateOfBirth.value), Number(luckyNumber.value));
-});
+checkNumber.addEventListener("click", checkIfBirthdayIsLucky);
+
+function checkIfBirthdayIsLucky() {
+  const dob = dateOfBirth.value;
+  const sum = calculateSum(dob);
+
+  console.log(sum);
+}
+
+function calculateSum(dob) {
+  dob = dob.replaceAll("-", "");
+
+  var sum = 0;
+  for(var i=0; i<dob.length; i++){
+    sum = sum + Number(dob.charAt(i));
+  }
+
+  return sum;
+}
